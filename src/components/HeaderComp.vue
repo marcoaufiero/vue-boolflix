49d18cf09ac6616af9f712c6c1a4de22af52bd2f
marcoaufiero <marcoaufiero96@gmail.com>
2022-11-29 11:41:04 +0100

    header style added

--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -1,6 +1,14 @@
 <template>
     <header>
-
+        <nav>
+            <div>
+                <h1>BOOLFLIX</h1>
+            </div>
+            <div>
+                <span>Cerca:</span>
+                <input class="searchbar" type="text">
+            </div>
+        </nav>
     </header>
 </template>
 
@@ -11,5 +19,39 @@
 </script>
 
 <style lang="scss" scoped>
+
+    header{
+        
+        background-color: black;
+        
+        nav{
+        display: flex;
+        justify-content: space-between;
+        padding: 20px;
+
+        h1{
+            color: red;
+            font-size: 2rem;
+        }
+
+        span{
+            color: white;
+            font-weight: 600;
+            margin-right: 10px;
+        }
+
+        .searchbar{
+            width: 200px;
+            height: 30px;
+            border: none;
+            font-size: 1.1rem;
+            padding: 5px 10px;
+        }
+
+    }
+
+
+    }
+    
 
 </style>
