47303868959b3483fa1ca34b54a4cb3a97795594
marcoaufiero <marcoaufiero96@gmail.com>
2022-11-29 13:13:20 +0100

    dynamic link data added to vue.app

--- src/App.vue
+++ src/App.vue
@@ -8,14 +8,35 @@
 <script>
 import HeaderComp from './components/HeaderComp.vue'
 import MainComp from './components/MainComp.vue'
-// import axios from 'axios'
+import axios from 'axios'
 
 export default {
   name: 'App',
   components: {
     HeaderComp,
     MainComp
+  },
+  
+  data(){
+    return{
+      dataApi: [],
+      userSearch: 'batman',
+      apiPath: {
+        basePath: 'https://api.themoviedb.org/3/search/',
+        moviePath: 'movie',
+        tvPath: 'tv',
+        apiKey: '?api_key=eb79889aac5d560eff7a8e0e1277ccf2',
+      },
+    } 
+  },
+  
+  mounted(){
+    axios.get(`${this.apiPath.basePath}${this.apiPath.moviePath}${this.apiPath.apiKey}&query=${this.userSearch}`)
+    .then((response) =>{
+    this.dataApi = response.data.results
+    })
   }
+
 }
 </script>
 
